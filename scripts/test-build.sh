59bd25687e3b14540e3338d318024d1ee42a7a83
Justin Schrader <jschrader@newrelic.com>
2020-03-31 11:27:45 -0700

    test: add test all script

---
+++ scripts/test-build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# runs the tests in ./src/index.test.js, but
+# replaces the import to target the compiled builds
+# in ./dist/es/index.js, ./dist/umd/index.js, and ./dist/cjs/index.js
+# this ensures that the act of compiling doesn't break the
+# expected behavior somehow.
+
+set -e
+
+printf "\n====\nTesting CJS dist build\n====\n" && \
+TEST_BUILD=cjs yarn test --coverage=0 && \
+printf "\n====\nTesting UMD dist build\n====\n" && \
+TEST_BUILD=umd yarn test --coverage=0 && \
+printf "\n====\nTesting ES dist build\n====\n" && \
+TEST_BUILD=es yarn test --coverage=0
